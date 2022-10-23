f599152debd0aca1e0de45ccad50014147133840
namithagunasekaran <namithag23@gmail.com>
2022-10-23 20:54:31 +0530

    next-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,11 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     reactStrictMode: true,
-//     swcMinify: true,
-//     compiler: {
-//         // Enables the styled-components SWC transform
-//         styledComponents: true,
-//     },
-// };
-
-// module.exports = nextConfig;
-/**
-   * @type {import('next').NextConfig}
-   */
- const nextConfig   = {
-    images: {
-      loader: 'akamai',
-      path: '',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+        // Enables the styled-components SWC transform
+        styledComponents: true,
     },
-    assetPrefix: './',
-  };
-  
-  export default   nextConfig;
+};
+
+module.exports = nextConfig;
